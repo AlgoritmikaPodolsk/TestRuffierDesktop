c0d0f6f27a9f47fa67270dddef4ee923b2f791bf
romka0830 <104301125+romka0830@users.noreply.github.com>
2022-05-29 12:45:12 +0300

    Update my_app.py
    last v.

--- my_app.py
+++ my_app.py
@@ -1,5 +1,5 @@
 from instr import *
-from final_win import TestWin
+from second_win import TestWin
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
 class MainWin(QWidget):
